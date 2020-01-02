af4baabb679c817c5cfd347cda106e44e882d7f7
random <80976700@qq.com>
2020-01-02 18:21:48 +0800

    修改了读取数据集雾气浓度统计数据的python文件

--- count_hazy.py
+++ count_hazy.py
@@ -1,11 +1,15 @@
 import os
 
-path = '/data/nyu/shallow_nyu/'
+path = '/input/data/shallow_nyu/'
 path_list = os.listdir(path)
 print(path_list)
 
+count = [0] * 12
 for i in path_list:
     a = path + i
     name_list = os.listdir(a)
     for j in name_list:
-        b = float(j[-8:-4])
+        b = int(float(j[-8:-4]) * 10 - 5)
+        print(b)
+        count[b] += 1
+print(count)
