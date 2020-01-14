7f40b3951dc5dff887f8d1a08a864eb6bb21e952
random <80976700@qq.com>
2020-01-14 16:16:09 +0800

    对裁切好以后的NTIRE2018数据集做了划分，按照811的比例分为train，val，test三个数据集

--- dataset_div.py
+++ dataset_div.py
@@ -1,52 +1,52 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#写一个给数据集划分为训练集,验证集,测试集的code
+# 写一个给数据集划分为训练集,验证集,测试集的code
+import os
+import shutil
 
-import os,shutil
+data_path = '/input/data/cut_ntire_2018/hazy/'  # 数据集的目录
 
-data_path='./haze/'#数据集的目录
-
-train_path='/input/ednet/dehaze_data/train/'
-validation_path='/input/ednet/dehaze_data/validation/'
-test_path='/input/ednet/dehaze_data/test/'
+train_path = '/input/data/ntire_2018/train/'
+val_path = '/input/data/ntire_2018/val/'
+test_path = '/input/data/ntire_2018/test/'
 if not os.path.isdir(train_path):
-	os.makedirs(train_path)
-if not os.path.isdir(validation_path):
-	os.makedirs(validation_path)
+    os.makedirs(train_path)
+if not os.path.isdir(val_path):
+    os.makedirs(val_path)
 if not os.path.isdir(test_path):
-	os.makedirs(test_path)
+    os.makedirs(test_path)
 
-train=0.8
-validation=0.1
-test=0.1
-data=os.listdir(data_path)
-data.sort(key = lambda x: int(x[:-30]))
+train = 0.8
+validation = 0.1
+test = 0.1
+data = os.listdir(data_path)
+data.sort(key=lambda x: int(x[:-4]))
 print(data)
-length=len(data)
+length = len(data)
 
-print('length:'+str(length))
+print('length:' + str(length))
 
-train_size=train*length
-validation_size=validation*length
-test_size=test*length
-print('train_size:'+str(train_size))
-print('validation_size:'+str(validation_size))
-print('test_size:'+str(test_size))
+train_size = train * length
+validation_size = validation * length
+test_size = test * length
+print('train_size:' + str(train_size))
+print('validation_size:' + str(validation_size))
+print('test_size:' + str(test_size))
 train_count = 0
 validation_count = 0
 test_count = 0
 for i in range(length):
-	if i % 1000 == 0:
-		print(i)
-	if i < train_size:
-		shutil.copy(data_path+data[i], train_path)
-		train_count += 1
-	elif i < train_size + validation_size:
-		shutil.copy(data_path+data[i], validation_path)
-		validation_count += 1
-	else:
-		shutil.copy(data_path+data[i], test_path)
-		test_count += 1
+    if i % 1000 == 0:
+        print(i)
+    if i < train_size:
+        shutil.copy(data_path + data[i], train_path)
+        train_count += 1
+    elif i < train_size + validation_size:
+        shutil.copy(data_path + data[i], val_path)
+        validation_count += 1
+    else:
+        shutil.copy(data_path + data[i], test_path)
+        test_count += 1
 print('train_count' + str(train_count))
 print('validation_count' + str(validation_count))
 print('test_count' + str(test_count))
